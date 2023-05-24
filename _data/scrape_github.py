d6f5a2d11a659f05b421800851ab97f0628ad593
nate stemen <nate@unitary.fund>
2023-05-23 19:57:12 -0700

    add bounties page + infra

---
+++ _data/scrape_github.py
@@ -0,0 +1,64 @@
+import json
+import os
+import re
+
+import frontmatter
+import github
+
+g = github.Github(os.getenv("GITHUB_TOKEN"))
+
+PROJECT_PATH = "../projects"
+
+
+def URL_to_repo_key(url: str) -> str:
+    repo = re.match("^https:\/\/github\.com\/([\w-]*\/[^\/]*)\/?$", url)
+    if repo:
+        return repo[1]
+    raise ValueError(f"did not find string match for {url}")
+
+
+def get_project_info():
+    for filename in os.listdir(PROJECT_PATH):
+        if filename.endswith(".md"):
+            yield frontmatter.load(os.path.join(PROJECT_PATH, filename))
+
+
+projects = {}
+for project in get_project_info():
+    project_url = project["project_url"]
+    main_project_repo_key = URL_to_repo_key(project_url)
+    project_name = main_project_repo_key.split("/")[-1].lower()
+    issue_list = []
+    amount_available = 0
+    total_bounty_amount = 0
+    for bounty in project.get("bounties", []):
+        total_bounty_amount += bounty["value"]
+        repo_key = bounty.get("repo") or main_project_repo_key
+        repo = g.get_repo(repo_key)
+
+        issue_num = bounty["issue_num"]
+        issue = repo.get_issue(number=issue_num)
+        if issue.state == "open":
+            amount_available += bounty["value"]
+        issue_list.append(
+            {
+                "title": issue.title,
+                "state": issue.state,
+                "assignees": [hacker.login for hacker in issue.assignees],
+                "value": bounty["value"],
+                "url": f"https://github.com/{repo_key}/issues/{issue_num}",
+            }
+        )
+
+    projects[project_name] = {
+        "name": project["title"],
+        "emoji": project.get("emoji", ""),
+        "url": project["project_url"],
+        "bounties": issue_list,
+        "amount_available": amount_available,
+        "total_amount": total_bounty_amount,
+    }
+
+
+with open("gh.json", "w") as f:
+    json.dump(projects, f, indent=2)
---
+++ pages/bounties.md
@@ -0,0 +1,12 @@
+---
+layout: layouts/bounties.njk
+title: Hackathon Bounties
+date: 2023-02-24
+permalink: /bounties/index.html
+metaDescription: All unitaryHACK participating bounties
+subtitle: Check out all of the quantum open source bounties that are a part of this year's unitaryHACK hackathon!
+emoji: 💻
+eleventyNavigation:
+  key: Bounties
+  order: 4
+---
